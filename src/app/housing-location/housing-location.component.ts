796dc6534fcb5ce6219e7cc45fd42849661d6d03
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-08-03 20:48:07 -0300

    feat: style hosuing location

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [],
-  templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  // templateUrl: './housing-location.component.html',
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        <img alt="Location pin" src="/images/location-pin.svg" />
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
+  styleUrl: './housing-location.component.css',
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }

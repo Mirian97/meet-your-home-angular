8484f01457011e6ab89f3edc15d21339367c771e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-08-03 22:09:26 -0300

    feat: include servide in details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocation: HousingLocation | undefined;
+  housingService = inject(HousingService);
 
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
+  }
 }

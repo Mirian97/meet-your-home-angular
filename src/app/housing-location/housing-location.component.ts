dd476af932ff20f7be58902dd8182301c3d56272
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-08-03 22:07:59 -0300

    feat: add link with router link from RouterModule

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   // templateUrl: './housing-location.component.html',
   template: `
     <section class="listing">
@@ -20,6 +21,7 @@ import { HousingLocation } from '../housing-location';
         <img alt="Location pin" src="/images/location-pin.svg" />
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css',
